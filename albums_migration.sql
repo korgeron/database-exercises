5744bbbd7e8365aff3218e4bed99812dfd29355d
orgeron.kevin <orgeron.kevin@gmail.com>
2022-07-06 16:57:57 -0500

    end of day

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,20 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(100) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    release_date DATE NOT NULL,
-    sales DOUBLE NOT NULL,
-    genre VARCHAR(100) NOT NULL,
+    id INT UNSIGNED AUTO_INCREMENT,
+    artist VARCHAR(100),
+    name VARCHAR(100),
+    release_date YEAR UNSIGNED,
+    sales DOUBLE UNSIGNED,
+    genre VARCHAR(100),
     PRIMARY KEY(id)
 );
+
+
+-- ADDING VALUES TO CODEUP_TEST_DB
+-- INSERT INTO albums(
+--     artist, name, genre
+-- )
+-- VALUES('the artist', 'the man kevin', 'ya boy ya kna!'),
+--         ('new artist', 'not kevin', 'not ya boy you know and love'),
+--        ('old artist', 'kevs the name', 'being back is the game');
