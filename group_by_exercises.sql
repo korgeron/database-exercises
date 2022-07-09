24b6e18d5285e37964a9bffe2e08aab6b52cb6a1
orgeron.kevin <orgeron.kevin@gmail.com>
2022-07-09 01:41:35 -0500

    finished exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -19,11 +19,19 @@ group by first_name, last_name
 order by last_name;
 
 # TODO: PROBLEM 4
-
+select distinct last_name
+from employees
+where last_name like('%q%') and last_name not like ('%qu%');
 
 # TODO: PROBLEM 5
-
+select distinct last_name, count(last_name) as similar_name
+from employees
+where last_name like ('%q%') and last_name not like ('%qu%')
+group by last_name;
 
 # TODO: PROBLEM 6
-
+select gender, count(gender) as count
+from employees
+where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya'
+group by gender;
 
